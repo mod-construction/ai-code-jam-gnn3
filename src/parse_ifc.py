fdec2cd033110622ed3b1a8d577b1583a5ca9af6
cramonell <cramonell@hotmail.com>
2025-10-16 18:10:45 +0200

    objects-parsed_0

--- src/parse_ifc.py
+++ src/parse_ifc.py
@@ -1,20 +1,48 @@
 # parse_ifc.py
 
 import ifcopenshell
+import ifcopenshell.util.element as Element
+import json
 
 def parse_ifc_to_json(ifc_path):
     model = ifcopenshell.open(ifc_path)
 
-    elements = {
-        "walls": [],
-        "doors": [],
-        "rooms": []
-    }
+    output = {
+        'schema': model.schema,
+        'entities': [],
+        'relationshipts': []
+        }
+    
+    # Entities
+    entity_types = ['IfcWall', 'IfcDoor']
+    for entity_type in entity_types:
+        entities = model.by_type(entity_type)
+        for entity in entities:
+            obj = {
+                'id': entity.GlobalId,
+                'name': entity.Name,
+                'tag': entity.Tag,
+            }
+            psets = Element.get_psets(entity)
+            # Add all properties and quantities as flat key-value pairs
+            for pset_name, properties in psets.items():
+                for prop_name, prop_value in properties.items():
+                # Skip the 'id' key that IfcOpenShell adds
+                    if prop_name != 'id':
+                        obj[prop_name] = prop_value
 
-    # TODO: Extract and transform relevant IFC elements into simplified JSON format
-    print("IFC file loaded. Found:")
-    print(" - Walls:", len(model.by_type("IfcWall")))
-    print(" - Doors:", len(model.by_type("IfcDoor")))
-    print(" - Spaces:", len(model.by_type("IfcSpace")))
+            output["entities"].append(obj)
 
-    return elements  # return empty for now
+
+
+    # relationships
+
+    # topology
+
+    return output
+
+result = parse_ifc_to_json("data/sample.ifc")
+
+# Save to JSON file
+with open("data/json_cr.json", "w", encoding="utf-8") as f:
+    json.dump(result, f, indent=2, ensure_ascii=False)
